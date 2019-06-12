212ffe3d6afcd1376f13d5aded30004fb61c3ab6
lorecast162 <lorecastcauli@gmail.com>
2019-06-12 19:33:06 +0200

    you know the drill

---
+++ JS/stuff/pixTst/pixTst.js
@@ -0,0 +1,10 @@
+window.onload = main();
+function main() {
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    var width = canvas.width, height = canvas.height;
+    var imagedata = context.createImageData(width,height);
+    
+    var r,g,b;
+
+}
---
+++ JS/stuff/rndTable/rndTable.js
@@ -0,0 +1,20 @@
+var str;
+var el="t";
+var i=0;
+var j=0;
+
+while (str < 2000) {
+    j =  rnd(1,3);
+    i =  rnd(1,3);
+
+    el = "t" + j + i;
+    document.getElementById(el).innerHTML = (2);
+    str++; 
+    
+    
+    
+}
+
+function rnd(min, max) {
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
