4a51927afa26c4a9f7a871d4ee4f303508f709f0
lorecast162 <lorecastcauli@gmail.com>
2019-08-10 01:22:54 +0200

    smores

--- CXX/C/cc65/C64/firstTst/main.c
+++ CXX/C/cc65/C64/firstTst/main.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <peekpoke.h>
+#include <c64.h>
+#include <stdlib.h>
+
+void wait(int wt);
 
 int main() {
   printf("UR MOM GAE HAHAAHA");
+  while (1) {
+    POKE(0xD020, rand());
+    wait(200);
+  }
   return 0;
+}
+
+
+void wait(int wt) {
+  int x=0;
+  while (x < wt){
+
+    x++;
+  }
 }
