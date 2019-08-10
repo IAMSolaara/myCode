2c01292a169ef1a7665c24ff339297c5b5b74499
lorecast162 <lorecastcauli@gmail.com>
2019-08-10 14:09:02 +0200

    asd

--- CXX/C/cc65/C64/firstTst/main.c
+++ CXX/C/cc65/C64/firstTst/main.c
@@ -5,13 +5,22 @@
 
 void wait(int wt);
 
-int main() {
+
+int main(){
+  int x = 54;
+  while (1) {
+    scanf("%d", &x);
+    printf("%d\n", x);
+  }
+  return 0;
+}
+
+void gae() {
   printf("UR MOM GAE HAHAAHA");
   while (1) {
     POKE(0xD020, rand());
     wait(200);
   }
-  return 0;
 }
 
 
