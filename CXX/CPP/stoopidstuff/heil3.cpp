94110c2670a18e4fa60217838c5e78a963c48eea
lorecast162 <lorecastcauli@gmail.com>
2019-08-10 22:54:11 +0200

    finally updated one of my first program with a change I thought was added but it wasn't. My life is, again, a lie.

---
+++ CXX/CPP/stoopidstuff/heil3.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+string dolphy;
+cout << "#_________#\n";
+cout << "|         |\n";
+cout << "| #  #### |\n";
+cout << "| #  #    |\n";
+cout << "| #  #    |\n";
+cout << "| ####### |\n";
+cout << "|    #  # |\n";
+cout << "|    #  # |\n";
+cout << "| ####  # |\n";
+cout << "#_________#\n";
+// larghezza heil 45 car;
+cout << "#__________________________________________#\n";
+cout << "|                                          |\n";
+cout << "| #   #    ####    #    #                  |\n";
+cout << "| #   #    #       #    #                  |\n";
+cout << "| #####    ####    #    #                  |\n";
+cout << "| #   #    #       #    #                  |\n";
+cout << "| #   #    ####    #    ####               |\n";
+cout << "|                                          |\n";
+cout << "| #   #   #    #####   #      ####   ####  |\n";
+cout << "| #   #   #      #     #      #      #   # |\n";
+cout << "| #####   #      #     #      ####   ####  |\n";
+cout << "| #   #   #      #     #      #      #   # |\n";
+cout << "| #   #   #      #     ####   ####   #   # |\n";
+cout << "#__________________________________________#\n";
+cout << "Heil?";
+cin >> dolphy;
+if ( dolphy == "Hitler" ) {
+		cout << "\nBrafen Racazzen\n";
+	}
+	else {
+		cout << "\nFICKEN COMMUNISTEN\n";
+	}
+}
