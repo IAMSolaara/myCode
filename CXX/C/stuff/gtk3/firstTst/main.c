2fbb95edd8a2d6f22911066e092ef192daa12f71
lorecast162 <lorecastcauli@gmail.com>
2019-08-11 14:58:59 +0200

    2

--- CXX/C/stuff/gtk3/firstTst/main.c
+++ CXX/C/stuff/gtk3/firstTst/main.c
@@ -1,7 +1,32 @@
 #include <gtk/gtk.h>
 
+static void print_hello(GtkApplication *app, gpointer user_data);
+static void activate(GtkApplication *app, gpointer user_data);
+
+int main(int argc, char **argv){
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new("com.mentalabs.firstTst", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
+  status = g_application_run(G_APPLICATION(app), argc, argv);
+  g_object_unref(app);
+
+  return status;
+}
+
 static void print_hello(GtkApplication *app, gpointer user_data){
-  g_print("ur mom gae.\n");
+  GtkWidget *window;
+  GtkWidget *label;
+
+  window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(window), "The Truth");
+  gtk_window_set_default_size(GTK_WINDOW(window), 200, 30);
+
+  label = gtk_label_new("ur mom asphalt");
+  gtk_container_add(GTK_CONTAINER(window), label);
+  
+  gtk_widget_show_all(window);
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
@@ -18,20 +43,8 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
   button = gtk_button_new_with_label("The truth");
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
-  g_signal_connect_swapped(button, "clicked", G_CALLBACK(gtk_widget_destroy), window);
+
   gtk_container_add(GTK_CONTAINER(button_box), button);
   
   gtk_widget_show_all(window);
-}
-
-int main(int argc, char **argv){
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new("com.mentalabs.firstTst", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
-
-  return status;
 }
