798c12ac8d6f126a63a4e382f34dfebb2a58655c
lorecast162 <lorecastcauli@gmail.com>
2019-08-13 19:31:44 +0200

    Commit through script

---
+++ Java/Java/stuff/firsties/classTst/main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args){
+	class1 test = new class1(123, 54353.345345f);
+	System.out.println(test.x);
+	System.out.println(test.y);
+	
+	
+	
+    }
+}
+
+
+public class class1 {
+    int x;
+    float y;
+    public class1(int arg1, float arg2){
+	this.x = arg1;
+	this.y = arg2;
+    }
+}
