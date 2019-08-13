bbacce63006e76a34c33dd538b1b8c55c4ad0df7
lorecast162 <lorecastcauli@gmail.com>
2019-08-13 20:44:26 +0200

    Commit through script

---
+++ Java/Java/stuff/firsties/classTst/class1.java
@@ -0,0 +1,10 @@
+package com.mentalabs;
+
+public class class1 {
+    public int x;
+    public float y;
+    public class1(int arg1, float arg2){
+	this.x = arg1;
+	this.y = arg2;
+    }
+}
--- Java/Java/stuff/firsties/classTst/main.java
+++ Java/Java/stuff/firsties/classTst/main.java
@@ -1,20 +1,9 @@
+import com.mentalabs.class1;
+
 public class main {
     public static void main(String[] args){
 	class1 test = new class1(123, 54353.345345f);
 	System.out.println(test.x);
 	System.out.println(test.y);
-	
-	
-	
-    }
-}
-
-
-public class class1 {
-    int x;
-    float y;
-    public class1(int arg1, float arg2){
-	this.x = arg1;
-	this.y = arg2;
     }
 }
