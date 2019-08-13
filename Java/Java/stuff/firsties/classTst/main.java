1506e3fbafaeba6adbc8f19f299c546816a854bc
lorecast162 <lorecastcauli@gmail.com>
2019-08-13 21:01:18 +0200

    Commit through script

--- Java/Java/stuff/firsties/classTst/main.java
+++ Java/Java/stuff/firsties/classTst/main.java
@@ -1,9 +1,26 @@
-import com.mentalabs.class1;
+
 
 public class main {
     public static void main(String[] args){
 	class1 test = new class1(123, 54353.345345f);
 	System.out.println(test.x);
 	System.out.println(test.y);
+	System.out.println(test.class1Ret());
+	System.out.println(test.class2Ret());
+    }
+}
+
+public class class1 {
+    int x;
+    float y;
+    public class1(int arg1, float arg2){
+	this.x = arg1;
+	this.y = arg2;
+    }
+    public int class1Ret(){
+	return x;
+    }
+    public float class2Ret(){
+	return y;
     }
 }
---
+++ Java/Java/stuff/firsties/extClassTst/main.java
@@ -0,0 +1,9 @@
+import com.mentalabs.class1;
+
+public class main {
+    public static void main(String[] args){
+	class1 test = new class1(123, 54353.345345f);
+	System.out.println(test.x);
+	System.out.println(test.y);
+    }
+}
