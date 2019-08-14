94b7b513a471bfb104b994fdca84cc3ae587ff78
lorecast162 <lorecastcauli@gmail.com>
2019-08-14 11:41:58 +0200

    Commit through script

---
+++ Java/Java/stuff/museo/Opera.java
@@ -0,0 +1,16 @@
+package com.mentalabs;
+
+public class Opera{
+    public String id;
+    public String sala;
+    public String nome;
+    public int anno;
+    public String autore;
+    public Opera(String idOpera, String salaOpera, String nomeOpera, int annoOpera, String autoreOpera){
+	this.id = idOpera;
+	this.sala = salaOpera;
+	this.nome = nomeOpera;
+	this.anno = annoOpera;
+	this.autore = autoreOpera;
+    }
+}
--- Java/Java/stuff/museo/main.java
+++ Java/Java/stuff/museo/main.java
@@ -1,32 +1,35 @@
 import java.io.*;
-import com.mentalabs.museo;
+import com.mentalabs.Opera;
+
+
 
 public class main {
-    public class statua extends museo {
-	String materiale;
-	public statua(String idStatua, String salaStatua, String nomeStatua, int annoStatua, String autoreStatua, String materialeStatua) {
-	    super(idStatua, salaStatua, nomeStatua, annoStatua, autoreStatua);
-	    this.materiale = materialeStatua;
-	}
+    
+    public static void main(String args[]) {
+	Dipinto Genti = new Dipinto("st001", "sala01", "Genti che danzano felicemente", 1945, "Adolf Hitler", "Olio su olio", 420, 69);
+		
     }
-    public class dipinto extends museo {
-	String tecnica;
-	int base;
-	int altezza;
-	public dipinto(String idDipinto, String salaDipinto, String nomeDipinto, int annoDipinto, String autoreDipinto, String tecnicaDipinto, int baseDipinto, int altezzaDipinto) {
+    public class Dipinto extends Opera {
+	private String tecnica;
+	private int base;
+	private int altezza;
+	public Dipinto(String idDipinto, String salaDipinto, String nomeDipinto, int annoDipinto, String autoreDipinto, String tecnicaDipinto, int baseDipinto, int altezzaDipinto) {
 	    super(idDipinto, salaDipinto, nomeDipinto, annoDipinto, autoreDipinto);
 	    this.tecnica = tecnicaDipinto;
 	    this.base = baseDipinto;
 	    this.altezza = altezzaDipinto;
 	}
     }
-    public static void main(String args[]) {
-	dipinto Genti = new dipinto("st001", "sala01", "Genti che danzano felicemente", 1945, "Adolf Hitler", "Olio su olio". 420d, 69d);
-	
-	
-	
-	
-	
-	
+    public class Statua extends Opera {
+	private String materiale;
+	private Statua(String idStatua, String salaStatua, String nomeStatua, int annoStatua, String autoreStatua, String materialeStatua) {
+	    super(idStatua, salaStatua, nomeStatua, annoStatua, autoreStatua);
+	    this.materiale = materialeStatua;
+	}
     }
+
+    
 }
+
+
+
