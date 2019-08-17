13adbcac6bfd2c4b4246894659a13dd9e162cb92
lorecast162 <lorecastcauli@gmail.com>
2019-08-17 22:37:39 +0200

    Commit through script

--- cxx/cpp/stuff/stdcpp/classTst/main.cpp
+++ cxx/cpp/stuff/stdcpp/classTst/main.cpp
@@ -1,56 +1,5 @@
 #include <iostream>
-
-class CalcClass {
-private:
-  double regX;
-  double regY;
-  char op;
-  double res;
-public:
-  CalcClass() {
-    regX = regY = res = 0;
-    op = '+';
-  }
-  //put instructions
-  void putX(double in) {
-    regX = in;
-  }
-  void putY(double in) {
-    regY = in;
-  }
-  void putOp(char in) {
-    op = in;
-  }
-  //get instructions, useful for debugging
-  double getX() {
-    return regX;
-  }
-  double getY() {
-    return regY;
-  }
-  char getOp() {
-    return op;
-  }
-  void run(){
-    switch (op) {
-    case '+' :
-      res = regX + regY;
-      break;
-    case '-' :
-      res = regX - regY;
-      break;
-    case '*' :
-      res = regX * regY;
-      break;
-    case '/' :
-      res = regX / regY;
-      break;
-    }      
-  }
-  double getRes() {
-    return res;
-  }
-};
+#include "classes.h"
 
 int main() {
   CalcClass calc;
