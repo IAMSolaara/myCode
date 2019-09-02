980aae0f73350bea36e342cbfcd2285546b05597
lorecast162 <lorecastcauli@gmail.com>
2019-09-02 22:47:44 +0200

    Commit through script

--- cxx/c/stuff/gbdk/movingDude/main.c
+++ cxx/c/stuff/gbdk/movingDude/main.c
@@ -7,7 +7,7 @@ void main(){
   set_sprite_data(0, 8, dude);
   set_sprite_tile(0, 0);
   move_sprite(0, 75, 75);
-  set_sprite_tile(1, 2);
-  move_sprite(0, 75+8, 75);
+  //  set_sprite_tile(1, 2);
+  //move_sprite(0, 75+8, 75);
   SHOW_SPRITES;
 }
