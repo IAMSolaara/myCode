15eb6a17c62e0a01910ff434d31a52715f05b7cc
lorecast162 <lorecastcauli@gmail.com>
2019-09-09 09:58:23 +0100

    Commit through script

--- web/PHP/stuff/phpExperiments/index.php
+++ web/PHP/stuff/phpExperiments/index.php
@@ -1,4 +1,8 @@
 <?php
+	$file = fopen("intst.txt", "r") or die("Can't open file....");
+	echo fread($file, filesize("intst.txt"));
+	fclose($file);
 	echo "Welcome to my PHP experiments! Today is ";
 	echo date("d-m-Y");
+#	echo readfile("inLineHT.html");
 ?>
