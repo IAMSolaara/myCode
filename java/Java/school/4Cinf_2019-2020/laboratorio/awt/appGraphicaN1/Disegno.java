5f60b881b453f754b63e2ea06b2fcc7e9ad7f739
lorecast162 <lorecastcauli@gmail.com>
2019-11-15 14:28:08 +0100

    commit

--- java/Java/school/4Cinf_2019-2020/laboratorio/awt/appGraphicaN1/Disegno.java
+++ java/Java/school/4Cinf_2019-2020/laboratorio/awt/appGraphicaN1/Disegno.java
@@ -18,8 +18,17 @@ public class Disegno extends Canvas {
 	public void paint(Graphics g){
 		g.drawLine((int)Math.round(p1.getX()), (int)Math.round(p1.getY()), (int)Math.round(p2.getX()), (int)Math.round(p2.getY()));
 
-		if ((int)Math.round(p1.getX()) >=  (int)Math.round(p1.getY()), (int)Math.round(p2.getX()), (int)Math.round(p2.getY()))
-		p1.trasla(0.1,0.1);
-		p2.trasla(0.1,0.1);
+		if (  (int)Math.round(p1.getX()) >= WIDTH  || 
+			  (int)Math.round(p1.getX()) <= 0      ||
+			  (int)Math.round(p1.getY()) >= HEIGHT ||
+			  (int)Math.round(p1.getY()) <= 0      ||
+
+			  (int)Math.round(p2.getX()) >= WIDTH  || 
+			  (int)Math.round(p2.getX()) <= 0      ||
+			  (int)Math.round(p2.getY()) >= HEIGHT ||
+			  (int)Math.round(p2.getY()) <= 0         ) {
+			p1.trasla(0.1,0.1);
+			p2.trasla(0.1,0.1);
+		}
 	}
 }
