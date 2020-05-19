b5617f69945b9f6cfa82ef1b65b2dae4440c55dd
lorecast162 <lorecastcauli@gmail.com>
2020-05-19 16:03:38 +0200

    python course

---
+++ python/Python3/school/4Cinf_2019-2020/pythonSensei/main.py
@@ -0,0 +1,84 @@
+import sys as sistema
+
+from requests import *
+
+import requests
+
+"""array = []
+x = 'roberto'
+if (x == 'cauli' or x =='ciao'):
+    print('giusto')
+else:
+    print('sbagliato')
+String[] cose = new String[10]
+
+for (String el : cose) {
+
+}
+
+cose = ['ciao', 'test', 'testsetest']
+for el in cose:
+    print(el)
+
+for el in cose:
+    el = el * 2
+    print(el)
+
+cose21 = range(3, 12, 2)
+for el in cose21:
+    print(el)
+
+for x in range(3, 5, 12):
+    print(x)
+
+done = 3
+while (done < 12):
+    print(done)
+    done += 1
+
+def potenza(x=2, esp=1):
+    return x ** esp
+
+print(potenza(12, esp=12))
+
+richiesta = get('https://mentalabs.eu/schoolSchedule/api/?day=Monday')
+print(richiesta.json())
+
+coso = {
+    'test' : 'testttsetet',
+    2 : 'ldksfjghldhfglksdjfgh',
+    'ciao' : [1,2,3,4],
+    'level1' : {
+        'level2': 'scendo'
+    }
+}
+
+print(coso[2])
+
+array = ['test', 'ciao', 'test']
+print(array)
+
+if ('test' in array):
+    array.remove('test')
+
+print(array)
+
+class testamento():
+    def __init__(self, x=2):
+        self.__x = x
+        print(self.__x)
+    def muoro(self):
+        self.__mangle()
+        print('sono muorot')
+    def getX(self):
+        return self.__x
+    def __mangle(self):
+        self.__x = 135174634909859870928740589
+
+testino = testamento('ciao')
+print(testino.getX())
+
+testino.muoro()
+print(testino.getX())
+
+"""
